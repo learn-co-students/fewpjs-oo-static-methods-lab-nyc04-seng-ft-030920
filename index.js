6505b34ae412ab65e4c76379d475542b16da046c
replacem3nts <donny.landis@gmail.com>
2020-05-06 15:16:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+  }
+  
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+  
+  static titleize(str) {
+    let dontCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let strArray = str.split(" ")
+    strArray[0] = this.capitalize(strArray[0])
+    return strArray.map((word) => dontCap.includes(word) ? word : Formatter.capitalize(word)).join(" ")
+  }
+
 }
